1f0c9ad0ac61fe74081a4fc4cf3bdcfba3a303c1
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-09-28 14:52:54 +0900

    appude-to

--- 2132134/src/enshu2_2022/kuku.java
+++ 2132134/src/enshu2_2022/kuku.java
@@ -6,17 +6,24 @@ public class kuku {
 		int x, y;
 		y = 1;
 		
-		while(y < 10) {
+		System.out.println(" | 1  2  3  4  5  6  7  8  9");
+		System.out.println("----------------------------");
+		for(y = 1; y < 10;y++) {
+			System.out.print(y + "|");
 			for(x = 1; x < 10; x++) {
-		
-				if(x * y < 10) {
-					System.out.print(" " + x * y  + " ");
+				
+				if(x >= y) {
+					if(x * y < 10) {
+						System.out.print(" " + x * y  + " ");
+					} else {
+						System.out.print(x * y + " ");
+					}
+					
 				} else {
-					System.out.print(x * y + " ");
+					System.out.print("   ");
 				}
 			}
 			System.out.println();
-			y++;
 		}
 	}
 }
