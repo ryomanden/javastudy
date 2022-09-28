35f2ff409c14837cb8f9e96fc84e64e3057bb10d
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-09-28 14:22:19 +0900

    First upload

---
+++ 2132134/src/enshu2_2022/kuku.java
@@ -0,0 +1,22 @@
+package enshu2_2022;
+
+public class kuku {
+	public static void main(String[] args) {
+		
+		int x, y;
+		y = 1;
+		
+		while(y < 10) {
+			for(x = 1; x < 10; x++) {
+		
+				if(x * y < 10) {
+					System.out.print(" " + x * y  + " ");
+				} else {
+					System.out.print(x * y + " ");
+				}
+			}
+			System.out.println();
+			y++;
+		}
+	}
+}
