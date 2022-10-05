3c03a167b73ba2056e083c755ccfc965c4366d44
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-05 14:37:16 +0900

    add PrintHex2

--- 2132134/src/enshu3_2022/kuku_hex.java
+++ 2132134/src/enshu3_2022/kuku_hex.java
@@ -4,23 +4,12 @@ public class kuku_hex {
 	public static void main(String[] args) {
 		int x, y, v, w;
 		y = 1;
-		
+
 		while(y < 16) {
 			for(x = 1; x < 16; x++) {
 				v = x * y;
-				
-				if(v < 16) {
-					System.out.print(" ");
-					printHex(v);
-					System.out.print(" ");
-			} else {
-					w = v / 16;
-					v = v % 16;
-					
-					printHex(w);
-					printHex(v);
-					System.out.print(" ");
-				}
+
+				printHex2(v);
 			}
 			System.out.println();
 			y++;
@@ -29,5 +18,15 @@ public class kuku_hex {
 	public static void printHex(int v) {
 		if(v < 10) System.out.print(v);
 		else	System.out.print((char)('a' + v - 10));
+	}
+
+	public static void printHex2(int v) {
+		int w;
+		w = v / 16;
+//		if(w != 0)	printHex(w);
+//		else System.out.print(" ");
+		printHex(w);
+		printHex(v % 16);
+		System.out.print(" ");
 	}
 }
