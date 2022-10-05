d5dbdd987e27ce7b1eabb9692fe6a5f448a3c7ad
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-05 15:21:41 +0900

    change header & add comments

--- 2132134/src/enshu3_2022/kuku_hex.java
+++ 2132134/src/enshu3_2022/kuku_hex.java
@@ -5,18 +5,21 @@ public class kuku_hex {
 
 		int y = 1;
 
-		header(16);
+		header(16,3);
 		while(y < 16) {
-			printHex3(y,2);
+			
+			printHex3(y,3); // print NUM
 			System.out.print( "| ");
-			for(int x = 1; x < 16; x++) {
-				printHex3(x * y,2);
+			
+			for(int x = 1; x < 16; x++) { // <--- MAIN
+				printHex3(x * y,3);
 			}
 			System.out.println();
 			y++;
 		}
 	}
 
+	/*--- method ---*/
 	public static void printHex(int v) {
 		if(v < 10) System.out.print(v);
 		else	System.out.print((char)('a' + v - 10));
@@ -41,11 +44,14 @@ public class kuku_hex {
 		System.out.print(" ");
 	}
 
-	public static void header(int y) {
-		System.out.print("   | ");
-		for(int i = 1; i < y; i++)	printHex2(i);
+	public static void header(int y,int n) {
+		for(int i = 0;i < n;i++)	System.out.print(" "); //add space to head
+		System.out.print(" | ");
+		
+		for(int i = 1; i < y; i++)	printHex3(i,n); //print NUM
 		System.out.println();
-		for(int i = 0; i < (y * 3 + 1); i++)	System.out.print("-");
+		
+		for(int i = 0; i < (y * (n+1) + 1); i++)	System.out.print("-"); //print "-"
 		System.out.println();
 	}
 }
