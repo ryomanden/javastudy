29069280fa4707545a4265a18e9fac2573d5923e
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-05 14:58:37 +0900

    add header

--- 2132134/src/enshu3_2022/kuku_hex.java
+++ 2132134/src/enshu3_2022/kuku_hex.java
@@ -2,31 +2,37 @@ package enshu3_2022;
 
 public class kuku_hex {
 	public static void main(String[] args) {
-		int x, y, v, w;
-		y = 1;
-
+		
+		int y = 1;
+		
+		header(16);
 		while(y < 16) {
-			for(x = 1; x < 16; x++) {
-				v = x * y;
-
-				printHex2(v);
+			printHex2(y);
+			System.out.print( "| ");
+			for(int x = 1; x < 16; x++) {
+				printHex2(x * y);
 			}
 			System.out.println();
 			y++;
 		}
 	}
+	
 	public static void printHex(int v) {
 		if(v < 10) System.out.print(v);
 		else	System.out.print((char)('a' + v - 10));
 	}
 
 	public static void printHex2(int v) {
-		int w;
-		w = v / 16;
-//		if(w != 0)	printHex(w);
-//		else System.out.print(" ");
-		printHex(w);
+		printHex(v / 16);
 		printHex(v % 16);
 		System.out.print(" ");
+	}
+	
+	public static void header(int y) {
+		System.out.print("   | ");
+		for(int i = 1; i < y; i++)	printHex2(i);
+		System.out.println();
+		for(int i = 0; i < (y * 3 + 1); i++)	System.out.print("-");
+		System.out.println();
 	}
 }
