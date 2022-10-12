54a0dcdb323293d7ec4f591fd37c4c40313aa842
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-12 14:54:10 +0900

    change to set &get

--- 2132134/src/enshu4_2022/SetGradeSample.java
+++ 2132134/src/enshu4_2022/SetGradeSample.java
@@ -2,29 +2,27 @@ package enshu4_2022;
 
 public class SetGradeSample {
 	public static void main(String[] args) {
-		CitStudent kodai = new CitStudent();
-		kodai.no = 2132999;
-		kodai.name = "工大  太郎";
-		kodai.year = 2;
-		kodai.grade = 80;
-		kodai.department = (kodai.no / 1000) % 10;
+		CitStudent2 kodai = new CitStudent2();
+		kodai.setNo(2132999);
+		kodai.setName("工大  太郎");
+		kodai.setYear(2);
+		kodai.setGrade(80);
 		showGrade(kodai);
 	}
 	
-	public static void showGrade(CitStudent data) {
-		String dep[] =  {"情報工学科","情報ネットワーク学科"};
+	public static void showGrade(CitStudent2 data) {
 		Character grade = 'D';
-		if(90 <= data.grade) {
+		if(90 <= data.getGrade()) {
 			grade = 'S';
-		}else if (80 <= data.grade) {
+		}else if (80 <= data.getGrade()) {
 			grade = 'A';
-		}else if (70 <= data.grade) {
+		}else if (70 <= data.getGrade()) {
 			grade = 'B';
-		}else if (60 <= data.grade) {
+		}else if (60 <= data.getGrade()) {
 			grade = 'C';
 		}else {
 			grade = 'D';
 		}
-		System.out.println("No: " +data.no+ " " +data.name+ " " +data.grade+ " " +dep[data.department-1]+ " " +grade);
+		System.out.println("No: " +data.getNo()+ " " +data.getName()+ " " +data.getGrade()+ " " +data.getDepartment()+ " " +grade);
 	}
 }
