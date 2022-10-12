a060a82f50d66840a4be22137c55c74d0e1e6c74
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-12 13:47:14 +0900

    add department output

--- 2132134/src/enshu4_2022/SetGradeSample.java
+++ 2132134/src/enshu4_2022/SetGradeSample.java
@@ -7,10 +7,12 @@ public class SetGradeSample {
 		kodai.name = "工大  太郎";
 		kodai.year = 2;
 		kodai.grade = 80;
+		kodai.department = (kodai.no / 1000) % 10;
 		showGrade(kodai);
 	}
 	
 	public static void showGrade(CitStudent data) {
-		System.out.println("No: " +data.no+ " " +data.name+ " " +data.grade);
+		String dep[] =  {"情報工学科","情報ネットワーク学科"};
+		System.out.println("No: " +data.no+ " " +data.name+ " " +data.grade+ " " +dep[data.department-1]);
 	}
 }
