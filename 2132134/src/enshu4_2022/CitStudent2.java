579371dad11d591d24678a3d4c37e1a79f91662b
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-12 15:50:37 +0900

    課題Iのcommitwasureta

--- 2132134/src/enshu4_2022/CitStudent2.java
+++ 2132134/src/enshu4_2022/CitStudent2.java
@@ -5,34 +5,40 @@ public class CitStudent2 {
 	String name;
 	int year;
 	int grade;
+	int department;
 	
+	int high, low, total ,num;
+	double ave;
+	
+	public CitStudent2() {
+		no = year = grade = department = high = low = total = num = 0;
+		ave = 0.0;
+		name = "";
+	}
+
+	CitStudent2(int p_no, String p_name){
+		no = p_no;
+		name = p_name;
+		year = grade = department = 0;
+	}
+	CitStudent2(int p_no, String p_name, int p_year){
+		no = p_no;
+		name = p_name;
+		year = p_year;
+		grade = department = 0;
+	}
+	
+	public void incNum() {
+		
+	}
 	public void setNo(int p_no) {
 		no = p_no;
 	}
-	public void setName(String p_name) {
-		name = p_name;
-	}
-	public void setYear(int p_year) {
-		year = p_year;
-	}
-	public void setGrade(int p_grade) {
-		grade = p_grade;
-	}
-	
+
 	public int getNo() {
 		return no;
 	}
-	public String getName() {
-		return name;
-	}
-	public int getYear() {
-		return year;
-	}
-	public int getGrade() {
-		return grade;
-	}
-	public String getDepartment() {
-		String dep[] =  {"情報工学科","情報ネットワーク学科"};
-		return dep[((no / 1000) % 10) - 1];
+	public int getNum() {
+		return no;
 	}
 }
--- 2132134/src/enshu4_2022/SetGradeSample.java
+++ 2132134/src/enshu4_2022/SetGradeSample.java
@@ -2,27 +2,27 @@ package enshu4_2022;
 
 public class SetGradeSample {
 	public static void main(String[] args) {
-		CitStudent2 kodai = new CitStudent2();
-		kodai.setNo(2132999);
-		kodai.setName("工大  太郎");
-		kodai.setYear(2);
-		kodai.setGrade(80);
+		CitStudent2 kodai = new CitStudent2(2132999,"工大  太郎");
+		// kodai.setNo();
+		// kodai.name = ;
+		kodai.year = 2;
+		kodai.grade = 80;
 		showGrade(kodai);
 	}
 	
 	public static void showGrade(CitStudent2 data) {
 		Character grade = 'D';
-		if(90 <= data.getGrade()) {
+		if(90 <= data.grade) {
 			grade = 'S';
-		}else if (80 <= data.getGrade()) {
+		}else if (80 <= data.grade) {
 			grade = 'A';
-		}else if (70 <= data.getGrade()) {
+		}else if (70 <= data.grade) {
 			grade = 'B';
-		}else if (60 <= data.getGrade()) {
+		}else if (60 <= data.grade) {
 			grade = 'C';
 		}else {
 			grade = 'D';
 		}
-		System.out.println("No: " +data.getNo()+ " " +data.getName()+ " " +data.getGrade()+ " " +data.getDepartment()+ " " +grade);
+		System.out.println("No: " +data.getNo()+ " " +data.name+ " " +data.grade);
 	}
 }
