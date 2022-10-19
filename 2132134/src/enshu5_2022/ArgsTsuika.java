40240e7f64caed2110801dddea7c0f805f3668d9
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-10-19 16:17:37 +0900

    add tsuika

---
+++ 2132134/src/enshu5_2022/ArgsTsuika.java
@@ -0,0 +1,9 @@
+package enshu5_2022;
+
+public class ArgsTsuika {
+	public static void main(String[] args) {
+		for(int i = (args.length - 1); i >= 0; i--) {
+			System.out.println(args[i]);
+		}
+	}
+}
