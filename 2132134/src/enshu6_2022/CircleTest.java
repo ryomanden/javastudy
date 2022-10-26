bedc70f36309e5a8b3ebd9f0b69d74f2fccae3cd
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-26 15:09:50 +0900

    add print instance

--- 2132134/src/enshu6_2022/CircleTest.java
+++ 2132134/src/enshu6_2022/CircleTest.java
@@ -4,11 +4,11 @@ public class CircleTest {
 	public static void main(String[] args) {
 		Circle c = new Circle();
 		c.move(100, 100);
-		
+		c.print();		
 		c.move(100, 100);
-		
+		c.print();		
 		c.moveto(100, 100);
-		
 		c.print();
+		
 	}
 }
