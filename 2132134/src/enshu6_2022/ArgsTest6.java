a09e0a7f54b469d83f2c4c04e62fd75d8e9e56d3
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-26 14:24:21 +0900

    change ArgsTest Main & 6 (q.42,43,44)

--- 2132134/src/enshu6_2022/ArgsTest6.java
+++ 2132134/src/enshu6_2022/ArgsTest6.java
@@ -2,10 +2,14 @@ package enshu6_2022;
 
 class ArgsTest6 {
 	int no;
+	static int count = 0;
 	public static void setNo(ArgsTest6 student, int p_no) {
 		student.no = p_no;
 		student.no++;
 		student = new ArgsTest6();
 		student.no = 2132777;
+	}
+	ArgsTest6(){
+		count++;
 	}
 }
--- 2132134/src/enshu6_2022/ArgsTestMain.java
+++ 2132134/src/enshu6_2022/ArgsTestMain.java
@@ -5,8 +5,10 @@ public class ArgsTestMain {
 		int i = 2132999;
 		ArgsTest6 kodai = new ArgsTest6();
 		ArgsTest6.setNo(kodai, i);
-		System.out.println("1: no = " +kodai.no+ " i = " +i);
+		System.out.println("1: no = " +kodai.no+ " i = " +i+ " ArgsTest6.count = " +ArgsTest6.count);
 		ArgsTest6.setNo(kodai, 2132888);
-		System.out.println("2: no = " +kodai.no+ " i = " +i);
+		System.out.println("2: no = " +kodai.no+ " i = " +i+ " ArgsTest6.count = " +ArgsTest6.count);
+		ArgsTest6.setNo(kodai, 2132888);
+		System.out.println("3: no = " +kodai.no+ " i = " +i+ " ArgsTest6.count = " +ArgsTest6.count);
 	}
 }
