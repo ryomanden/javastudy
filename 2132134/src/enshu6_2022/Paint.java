a3f34f4ae25375b990553477f8c18aa57bdcd416
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-26 15:38:06 +0900

    fix import

--- 2132134/src/enshu6_2022/Paint.java
+++ 2132134/src/enshu6_2022/Paint.java
@@ -1,9 +1,12 @@
 package enshu6_2022;
 
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Paint extends Frame implements MouseListener, MouseMotionListener {
 	int x, y;
@@ -12,7 +15,7 @@ public class Paint extends Frame implements MouseListener, MouseMotionListener {
 		Paint f = new Paint();
 		f.setSize(640,480);
 		f.setTitle("Paint Sample");
-		f,addWindowListener(new WindowAdapter() {
+		f.addWindowListener(new WindowAdapter() {
 			@Override public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
