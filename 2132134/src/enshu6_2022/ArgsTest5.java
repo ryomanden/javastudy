27dd2db15f65043367b1bd84d0e77e1d0c080036
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-26 13:40:51 +0900

    make ArgsTest5

---
+++ 2132134/src/enshu6_2022/ArgsTest5.java
@@ -0,0 +1,19 @@
+package enshu6_2022;
+
+public class ArgsTest5 {
+	int no;
+	public static void main(String[] args) {
+		int i = 2132999;
+		ArgsTest5 kodai = new ArgsTest5();
+		setNo(kodai, i);
+		System.out.println("1: no = " +kodai.no+ " i = " +i);
+		setNo(kodai, 2132999);
+		System.out.println("2: no = " +kodai.no+ " i = " +i);
+	}
+	public static void setNo(ArgsTest5 student, int p_no) {
+		student.no = p_no;
+		student.no++;
+		student = new ArgsTest5();
+		student.no  = 2132777;
+	}
+}
