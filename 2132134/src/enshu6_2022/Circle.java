55be07715535717e2b09d664aba2e1c8b1c4c207
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-26 14:38:34 +0900

    add class 'Coord, Circle'

---
+++ 2132134/src/enshu6_2022/Circle.java
@@ -0,0 +1,9 @@
+package enshu6_2022;
+
+public class Circle extends Coord {
+	int color, size;
+	Circle() {
+		color = 0;
+		size = 1;
+	}
+}
---
+++ 2132134/src/enshu6_2022/Coord.java
@@ -0,0 +1,8 @@
+package enshu6_2022;
+
+public class Coord {
+	int x, y;
+	Coord(){
+		x = y =0;
+	}
+}
