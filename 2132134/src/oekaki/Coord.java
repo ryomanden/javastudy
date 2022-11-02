9525ecd40515dc65ea75f00b29298a7ffc03565c
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-11-02 13:49:22 +0900

    changeto kadai U

--- 2132134/src/oekaki/Coord.java
+++ 2132134/src/oekaki/Coord.java
@@ -1,24 +1,13 @@
 package oekaki;
 
-import java.awt.Graphics;
-
 public class Coord {
 	int x, y;
 	Coord(){
-		x = y = 10;
+		x = y = 0;
 	}
 	public void move(int dx, int dy) {
 		x += dx;
 		y += dy;
-	}
-	public void moveto(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public void paint(Graphics g) {}
-	
-	public void print() {
-		System.out.println("x = " +this.x+ " y = " +this.y);
+		System.out.println("move1");
 	}
 }
