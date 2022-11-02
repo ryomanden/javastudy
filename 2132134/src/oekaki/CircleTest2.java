4a7bed206974f95e249c4726a59773744cda2bab
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-11-02 14:00:12 +0900

    changeto kadai V

--- 2132134/src/oekaki/CircleTest2.java
+++ 2132134/src/oekaki/CircleTest2.java
@@ -1,8 +1,12 @@
 package oekaki;
 
+import java.util.ArrayList;
+
 class CircleTest2 {
 	public static void main(String[] args) {
+		ArrayList<Coord> objList = new ArrayList<Coord>();
 		Coord c = new Circle();
+		objList.add(c);
 		c.move(100, 100);
 		System.out.println("x = " +c.x+ " y = " +c.y);
 		// c.setSize(20);
