953f3b85976f4fd9c6583c142aa6479d250d5b55
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-11-02 13:45:38 +0900

    create

--- 2132134/src/oekaki/CircleTest2.java
+++ 2132134/src/oekaki/CircleTest2.java
@@ -1,5 +1,9 @@
 package oekaki;
 
 class CircleTest2 {
-
+	public static void main(String[] args) {
+		Coord c = new Circle();
+		c.move(100, 100);
+		System.out.println("x = " +c.x+ " y = " +c.y);
+	}
 }
