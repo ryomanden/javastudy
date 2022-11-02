84e6318fa071fc874539c9746cdb1e85af55c794
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-11-02 14:21:35 +0900

    create

---
+++ 2132134/src/oekaki/Figure.java
@@ -0,0 +1,11 @@
+package oekaki;
+
+import java.awt.Graphics;
+
+public class Figure extends Coord {
+	int color;
+	Figure(){
+		color = 0;
+	}
+	public void paint(Graphics g) {}
+}
---
+++ 2132134/src/oekaki/Paint2.java
@@ -0,0 +1,5 @@
+package oekaki;
+
+class Paint2 {
+
+}
