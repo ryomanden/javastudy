ba992097ae6f6587833537ca7c7818d819b20f53
ryomanden <s2132134lj@s.chibakoudai.jp>
2022-11-23 14:05:54 +0900

    add print

--- 2132134/src/enshu10_2022/AppFrame.java
+++ 2132134/src/enshu10_2022/AppFrame.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 
 public class AppFrame extends Frame {
 	@Override public void paint(Graphics g) {
+		System.out.println("paint");
 		g.setColor(Color.red);
 		g.drawString("Hello Java!", 70, 120);
 		g.drawRect(50, 50, 100, 100);
