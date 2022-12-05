d880ed1d7b38534b0bc61da3fea36513c7f54fa2
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-12-06 02:13:48 +0900

    add super

--- 2132134/src/enshu10_2022/MenuBtn.java
+++ 2132134/src/enshu10_2022/MenuBtn.java
@@ -5,14 +5,9 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 
 class MenuBtn extends JButton {
-	public static void main(String[] args) {
-		
-	}
 	public MenuBtn(String text,String command) {
-		addActionListener(actionListener);
-		setText(text);
+		super(text);
 		setActionCommand(command);
 		setPreferredSize(new Dimension(80,40));
 	}
-
 }
